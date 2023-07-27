8d9635c804e33fdf0b86a7b546295056241280e2
bvulet <borisvuleta1@gmail.com>
2023-07-27 12:08:34 +0200

    configuring controller to composition ospaths

--- process/GetOsPaths.py
+++ process/GetOsPaths.py
@@ -99,34 +99,3 @@ class GetOsPaths:
             return(2)
 
 
-    def update_video_playlist(self, playlist_directory):
-
-        types = ('.mp4', '.avi')  # the tuple of file types
-        video_source = []
-        files_grabbed = []
-
-        if len(os.listdir(playlist_directory)) == 0:
-
-            mod_path = Path(__file__).parent.parent
-            playlist_directory = (mod_path / "def_dir" / "video_dir").resolve()
-            files_grabbed = os.listdir(str(playlist_directory))
-
-        else:
-
-            for file_type in os.listdir(playlist_directory):
-
-                if file_type.endswith(types):
-
-                    files_grabbed.extend([file_type])
-
-                else:
-
-                    mod_path = Path(__file__).parent.parent
-                    playlist_directory = (mod_path / "def_dir" / "video_dir").resolve()
-                    files_grabbed = os.listdir(str(playlist_directory))
-
-        for index in range(len(files_grabbed)):
-            value = files_grabbed[index]
-            video_source.extend([os.path.join(playlist_directory, value)])
-
-        return video_source
